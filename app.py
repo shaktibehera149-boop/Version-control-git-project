1a7c4c83afea0be9f6e8a391e598f2f5bc89d8ce
Shakti Kumar Behera <shaktibehera149@gmail.com>
2026-05-07 23:02:24 +0530

    added mongoDB backend

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+from pymongo import MongoClient
 import json
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://Admin:Admin123@cluster1.jumillq.mongodb.net/?appName=Cluster1")
+
+db = client["todoDB"]
+
+collection = db["items"]
 
 @app.route('/')
 def home():
@@ -14,6 +21,22 @@ def api():
         data = json.load(file)
 
     return jsonify(data)
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit():
+
+    itemName = request.form.get('itemName')
+
+    itemDescription = request.form.get('itemDescription')
+
+    data = {
+        "itemName": itemName,
+        "itemDescription": itemDescription
+    }
+
+    collection.insert_one(data)
+
+    return "Data submitted successfully"
 
 if __name__ == '__main__':
     app.run(debug=True)
